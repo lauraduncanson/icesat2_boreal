102d63dfe2838bcacd61d2153f7d30254a41042f
icesat2nb3 <root@workspacecij8l608uv6yiqqs.ws-56654bdc9-4cd7z>
2021-05-12 18:11:39 +0000

    first run.sh commit

---
+++ dps/alg_3-1-5/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# this is intended for running DPS jobs; the input directory is where a single file has been pulled because download=TRUE in the algorithm_config.yaml file
+
+mkdir output
+
+
+FILENAMELIST=$(ls -d input/*)
+
+basedir=$( cd "$(dirname "$0")" ; pwd -P )  # goes to alg_3-1-5/
+
+# First file in input/ dir
+INPUTFILE="${PWD}/${FILENAMELIST[0]}"
+
+#FILELIST=($INPUTFILE)
+
+## Hard coded args for each run (if any; usually just output dir)
+
+# Work dir is always from where your script is called
+# Base dir is always the relative dir within the run*.sh script
+
+# Absolute path here
+# This PWD is wherever the job is run (where the .sh is called from) 
+OUTPUTDIR="${PWD}/output"
+
+#cd ${basedir}/notebooks/3.Gridded product development/
+
+#TODO: 
+#python setup.py install -prefix=${basedir}/gedipy_bin
+
+#export PYTHONPATH="${PYTHONPATH}:${basedir}/notebooks/3.Gridded product development/"
+
+# Cmd line call that worked
+#python 3.1.5_dps.py --in_tile_fn '/projects/maap-users/alexdevseed/boreal_tiles.gpkg' --in_tile_num 30550 --tile_buffer_m 120 --in_tile_layer "boreal_tiles_albers" -o '/projects/tmp/Topo/'
+
+python ${basedir}/../../notebooks/3.Gridded_product_development/3.1.5_dps.py --in_tile_fn ${INPUTFILE} --in_tile_num $2 --tile_buffer_m $3 --in_tile_layer $4 -o $OUTPUTDIR
