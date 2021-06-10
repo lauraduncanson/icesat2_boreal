b387f6532c0adfac3c91ace08b3eb57403f90741
root <root@workspace457duvv4ywyp0tfd.ws-756f5d8cb6-hgz6h>
2021-06-10 19:07:44 +0000

    fixed choices bug in extract_atl08

--- lib/extract_atl08.py
+++ lib/extract_atl08.py
@@ -413,9 +413,19 @@ def main():
         def __init__(self, start, end):
             self.start = start
             self.end = end
+
         def __eq__(self, other):
-            return self.start <= other <= self.end                                             
-                                         
+            return self.start <= other <= self.end
+
+        def __contains__(self, item):
+            return self.__eq__(item)
+
+        def __iter__(self):
+            yield self
+
+        def __str__(self):
+            return '[{0},{1}]'.format(self.start, self.end)                                           
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", type=str, help="Specify the input ICESAT H5 file")
     parser.add_argument("-r", "--resolution", type=str, default='100', help="Specify the output raster resolution (m)")
