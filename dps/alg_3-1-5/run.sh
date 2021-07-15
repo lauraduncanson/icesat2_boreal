8e81ca300a5cccdb8fce6966e025610a8dd9636b
nathanmthomas <nathan.m.thomas@nasa.gov>
2021-07-15 13:58:35 +0000

    updated package versions

--- dps/alg_3-1-5/run.sh
+++ dps/alg_3-1-5/run.sh
@@ -4,7 +4,8 @@
 # This installs the python libs needed to run the script at the bottom
 # these libs are NOT included in the base image (vanilla: https://mas.maap-project.org/root/ade-base-images/-/blob/vanilla/docker/Dockerfile)
 #conda install -yq -c conda-forge geopandas rio-cogeo rio-tiler importlib_resources
-pip install --user -U numpy==1.20.3 geopandas rio-cogeo rio-tiler==2.0.8 importlib_resources
+pip install --user -U numpy==1.20.3 geopandas rio-cogeo rio-tiler==2.0.8 rasterio==1.2.6 importlib_resources
+#pip install --user -U /projects/requirements.txt
 
 mkdir output
 
