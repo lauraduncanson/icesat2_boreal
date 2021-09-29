3cc64eb47ea699e87683d48ec616d30b348e3e8d
lduncanson <lduncans@umd.edu>
2021-09-29 18:16:49 +0000

    prep for 3.4 dps

--- dps/alg_3-4/run_boreal_biomass.sh
+++ dps/alg_3-4/run_boreal_biomass.sh
@@ -14,28 +14,23 @@ conda install -c conda-forge -y r-gridExtra r-tidyverse r-randomForest r-raster 
 
 mkdir output
 
-#unpack biomass models tar
-tar -xvf input/bio_models.tar
-
-#FILENAME1=$(ls -d input/*.tar)
-FILENAME1=$(ls -d input/*.csv)
-FILENAME2=$(ls -d input/C*)
-FILENAME3=$(ls -d input/L*)
-
 # Note: the numbered args are fed in with the in_param_dict in the Run DPS chunk of 3.4_dps.ipynb
 
-#TAR_FILE=${4}
+ATL08_CSV=${1}#$(ls -d input/*.csv)
+TOPO_TIF=${2}#$(ls -d input/C*)
+LANDSAT_TIF=${3}#$(ls -d input/L*)
+TAR_FILE=${4}
+
+#unpack biomass models tar
+#tar -xvf input/bio_models.tar
 
 # This will put the *rds in the same dir as the R script
-#tar -xf ${TAR_FILE} -C ${basedir}
+tar -xf ${TAR_FILE} -C ${basedir}
 
 # This PWD is wherever the job is run (where the .sh is called from) 
 OUTPUTDIR="${PWD}/output"
 
-#command line call that works python /projects/icesat2_boreal/lib/extract_atl08.py -i "/projects/test_data/test_data_30m/ATL08_30m_20181014001049_02350102_003_01.h5" --no-filter-qual --do_30m
-
-Rscript mapBoreal.R $FILENAME1 $FILENAME2 $FILENAME3
-#Rscript ${basedir}/mapBoreal.R ${1} ${2} ${3}
-#gdal_translate output/*.tif -of COG
+echo Rscript mapBoreal.R ${ATL08_CSV} ${TOPO_TIF} ${LANDSAT_TIF}
+Rscript mapBoreal.R ${ATL08_CSV} ${TOPO_TIF} ${LANDSAT_TIF}
 
 
