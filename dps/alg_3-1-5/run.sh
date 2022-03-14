38fb3706b5bdb118a0884dbbd308c28299787efc
montesano <paul.montesano@gmail.com>
2022-03-14 13:50:02 +0000

    dps prep for topo and tile_atl08; trying pip with req main in run.sh with correct path to req main txt

--- dps/alg_3-1-5/run.sh
+++ dps/alg_3-1-5/run.sh
@@ -5,12 +5,14 @@ conda activate r-with-gdal
 # these libs are NOT included in the base image (vanilla: https://mas.maap-project.org/root/ade-base-images/-/blob/vanilla/docker/Dockerfile)
 #conda install -yq -c conda-forge geopandas rio-cogeo rio-tiler importlib_resources
 #pip install --user -U numpy==1.20.3 geopandas rio-cogeo rio-tiler==2.0.8 rasterio==1.2.6 importlib_resources
-pip install --user -U /projects/icesat2_boreal/dps/requirements_main.txt
+
 unset PROJ_LIB
 
 mkdir output
 
 basedir=$( cd "$(dirname "$0")" ; pwd -P )  # goes to alg_3-1-5/
+
+pip install --user -U ${basedir}/../requirements_main.txt
 
 # First file in input/ dir
 # TODO: Fragile relying on alphabetical order
