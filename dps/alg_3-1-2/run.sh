3a08ccab615945a598dc7378ee90395d20078507
nathanmthomas <nathan.m.thomas@nasa.gov>
2022-03-14 19:36:12 +0000

    updates to r:latest

--- dps/alg_3-1-2/run.sh
+++ dps/alg_3-1-2/run.sh
@@ -6,7 +6,7 @@
 #conda install -yq -c conda-forge geopandas rio-cogeo rio-tiler importlib_resources
 unset PROJ_LIB
 conda list
-#pip install --user -U numpy==1.20.3 geopandas==0.9.0 rio-cogeo==2.3.1 rio-tiler==2.1.4 rasterio==1.2.6 morecantile==2.1.4 pystac-client importlib_resources 
+pip install --user -U numpy==1.20.3 geopandas==0.9.0 rio-cogeo==2.3.1 rio-tiler==2.1.4 rasterio==1.2.6 morecantile==2.1.4 pystac-client importlib_resources 
 
 mkdir output
 
