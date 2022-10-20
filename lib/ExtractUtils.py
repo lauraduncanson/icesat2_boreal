7909875a827e8c83f650dffd6a5263cfa45d74fb
montesano <paul.montesano@gmail.com>
2022-10-20 15:47:41 +0000

    funcs for building ms comp params table

--- lib/ExtractUtils.py
+++ lib/ExtractUtils.py
@@ -5,6 +5,10 @@ from rasterio.crs import CRS
 import os
 import numpy as np
 
+import glob
+import s3fs
+s3 = s3fs.S3FileSystem(anon=True)
+
 import sys
 #sys.path.append('/projects/code/icesat2_boreal/notebooks/3.Gridded_product_development')
 #from CovariateUtils import *
@@ -463,6 +467,30 @@ def get_tile_matches_gdf(tindex_master_fn,
     
     return tile_index_matches_gdf
 
+def plot_gdf_on_world(gdf, DO_TYPE=True, boundary_layer_fn = '/projects/my-public-bucket/analyze_agb/input_zones/wwf_circumboreal_Dissolve.geojson', LIST_4326_VERTS = [(-180, 40), (-180, 78), (180, 78), (180, 40), (-180, 40)]):
+    
+    '''Plot a gdf (in 4326) on a world map'''
+    import shapely
+    gdf = gdf.to_crs("EPSG:4326")
+    
+    # Get world
+    world = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres") )
+
+    # Create a custom polygon
+    polygon = shapely.geometry.Polygon(LIST_4326_VERTS)
+    poly_gdf = geopandas.GeoDataFrame([1], geometry=[polygon], crs=world.crs)
+    
+    world_clip = geopandas.clip(world, poly_gdf)
+    ax = world_clip.plot(color='gray')
+    
+    # Get some boundary_layer
+    boundary_layer = geopandas.read_file(boundary_layer_fn)
+    ax = boundary_layer.boundary.plot(color='black', ax=ax)
+    if DO_TYPE:
+        print(gdf.plot(column='run_type', cmap = "nipy_spectral", legend=True,  ax=ax, figsize=(25,10)))
+    else:
+        print(gdf.plot(color='orange', ax=ax, figsize=(25,10)))
+
 
 def build_tiles_json(tile_index_matches_gdf, tindex_master_fn, SHOW_MAP=True):
     
@@ -495,7 +523,7 @@ def build_tiles_json(tile_index_matches_gdf, tindex_master_fn, SHOW_MAP=True):
 
         boreal = geopandas.read_file('/projects/my-public-bucket/analyze_agb/input_zones/wwf_circumboreal_Dissolve.geojson')
         ax = boreal.boundary.plot(color='black', ax=ax)
-        tile_matches_geojson_string.boundary.plot(color='orange', ax=ax, figsize=(20,5))
+        tile_matches_geojson_string.plot(color='orange', ax=ax, figsize=(25,10))
 
     tile_matches_geojson_string = tile_matches_geojson_string.to_json()
 
@@ -529,7 +557,6 @@ def build_mosaic_json(
     # Step 2 get the tiles json rfom the gdf of matched tiles
     tile_matches_geojson = build_tiles_json(tile_index_matches_gdf, tindex_master_fn, SHOW_MAP=True)
 
-
     print(f"Building {out_mosaic_json_fn}")
     mosaicdata = MosaicJSON.from_features(tile_matches_geojson.get('features'), minzoom=6, maxzoom=18, accessor=get_accessor)
 
@@ -537,4 +564,45 @@ def build_mosaic_json(
         mosaic.write(overwrite=True)
         
     return out_mosaic_json_fn, tile_index_matches_gdf
-        
+
+def build_json_mscomp_df(s3_path: str, mscomp_input_glob_str: str, mscomp_num_scenes_glob_str: str, params_cols_list: list):
+    
+    '''Build a single-row data frame of the input multi-spec compositing parameters for each tile'''
+    
+    dir_tile = os.path.split(s3_path)[0]
+    
+    # Find the json file with the MS comp input params
+    f = s3.glob(os.path.join(dir_tile, mscomp_input_glob_str))[0]
+    df = pd.read_json('s3://' + f, typ='series').to_frame().transpose()[params_cols_list]
+    df['json_path'] = s3_path
+    
+    # Find the json file with the metadata for each scene; get count of scenes used for this composite
+    f = s3.glob(os.path.join(dir_tile, mscomp_num_scenes_glob_str))[0]
+    #print(f)
+    scene_metadata_list = pd.read_json('s3://' + f, typ='series').features
+    df['num_scenes'] = len(scene_metadata_list)
+    
+    return df
+
+def write_mscomp_params_table(tindex_fn, MSCOMP_TYPE = 'HLS', mscomp_input_glob_str="output*context.json", mscomp_num_scenes_glob_str="master*.json", cols_list=['in_tile_num','max_cloud','start_month_day','end_month_day','start_year','end_year']):
+    
+    # Combine these cols to get run_type
+    params_cols_list = cols_list[1:]
+    
+    tindex = pd.read_csv(tindex_fn)
+    list_s3_paths = tindex.s3_path.to_list()
+    
+    # Concatenate all single-row data frames of MScomp input params into one
+    df = pd.concat([build_json_mscomp_df(s3_path, mscomp_input_glob_str, mscomp_num_scenes_glob_str, cols_list) for s3_path in list_s3_paths])#.reset_index(drop=True)
+    
+    df['run_type'] = df[params_cols_list].apply(
+                                                lambda x: '_'.join(x.dropna().astype(str)),
+                                                axis=1
+                                            )
+    OUTPUT_FN = os.path.join(os.path.dirname(tindex_fn), f'{MSCOMP_TYPE}_input_params.csv')
+    df.rename(columns={'in_tile_num': 'tile_num'}, inplace=True)
+    df.to_csv(OUTPUT_FN)
+    
+    print(f"Wrote MS comp {MSCOMP_TYPE} params table: {OUTPUT_FN}")
+    
+    return OUTPUT_FN
