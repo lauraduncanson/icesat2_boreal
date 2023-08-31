08ea9540f6c8ff7fc9bbbfee06675dcda372adee
montesano <paul.montesano@gmail.com>
2023-08-31 21:06:04 +0000

    dps prep; rm activate icesat2_boreal in build cmd

--- dps/build_command_main.sh
+++ dps/build_command_main.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -x
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
+
 #install requirements packages
 mamba env update -f ${basedir}/above_env.yml
+
 # needed for ee asset export
 mamba install -c conda-forge earthengine-api
 
 pushd ${HOME}
 
 # Do not remove this (PMM Dec 2022)
-source activate icesat2_boreal
+# Testing remove (PMM Aug 2023)
+#source activate icesat2_boreal
 
 pip install git+https://github.com/MAAP-Project/maap-py.git#egg=maappy
 
