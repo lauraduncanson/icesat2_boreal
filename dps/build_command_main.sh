9db04569754d3a3a5eb08454ba18196349568bf8
montesano <paul.montesano@gmail.com>
2023-09-01 01:39:28 +0000

    dps prep

--- dps/build_command_main.sh
+++ dps/build_command_main.sh
@@ -5,13 +5,13 @@ basedir=$( cd "$(dirname "$0")" ; pwd -P )
 #install requirements packages
 mamba env update -f ${basedir}/above_env.yml
 
-# needed for ee asset export
-mamba install --name base -c conda-forge earthengine-api
-
 pushd ${HOME}
 
 # Do not remove this (PMM Dec 2022)
 source activate icesat2_boreal
+
+# needed for ee asset export
+mamba install --name icesat2_boreal -c conda-forge earthengine-api
 
 pip install git+https://github.com/MAAP-Project/maap-py.git#egg=maappy
 
