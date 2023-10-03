7e6791a0d33d08c7b4182f318a634e06b09e9c74
montesano <paul.montesano@gmail.com>
2023-10-03 19:15:02 +0000

    cleanup gee subtile script comments

--- lib/do_gee_download_by_subtile.py
+++ lib/do_gee_download_by_subtile.py
@@ -16,6 +16,7 @@ import zipfile
 import glob
 
 def get_gee_assets(asset_path):
+    
     # fetch EE assets
     COLL_ID = asset_path
     collection = ee.ImageCollection(COLL_ID)
@@ -24,7 +25,9 @@ def get_gee_assets(asset_path):
     collection_items_df = pd.DataFrame.from_dict(collection_items)
 
     return collection_items_df
+
 def create_fishnet(collection_items_df, asset_num, dims, DEBUG=False):
+    
     in_crs = 'BOUNDCRS[SOURCECRS[PROJCRS["unnamed",BASEGEOGCRS["GRS 1980(IUGG, 1980)",DATUM["unknown",ELLIPSOID["GRS80",6378137,298.257222101,LENGTHUNIT["metre",1,ID["EPSG",9001]]]],PRIMEM["Greenwich",0,ANGLEUNIT["degree",0.0174532925199433]]],CONVERSION["unnamed",METHOD["Albers Equal Area",ID["EPSG",9822]],PARAMETER["Latitude of 1st standard parallel",50,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8823]],PARAMETER["Latitude of 2nd standard parallel",70,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8824]],PARAMETER["Latitude of false origin",40,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8821]],PARAMETER["Longitude of false origin",180,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8822]],PARAMETER["Easting at false origin",0,LENGTHUNIT["Meter",1],ID["EPSG",8826]],PARAMETER["Northing at false origin",0,LENGTHUNIT["Meter",1],ID["EPSG",8827]]],CS[Cartesian,2],AXIS["easting",east,ORDER[1],LENGTHUNIT["Meter",1]],AXIS["northing",north,ORDER[2],LENGTHUNIT["Meter",1]]]],TARGETCRS[GEOGCRS["WGS 84",DATUM["World Geodetic System 1984",ELLIPSOID["WGS 84",6378137,298.257223563,LENGTHUNIT["metre",1]]],PRIMEM["Greenwich",0,ANGLEUNIT["degree",0.0174532925199433]],CS[ellipsoidal,2],AXIS["geodetic latitude (Lat)",north,ORDER[1],ANGLEUNIT["degree",0.0174532925199433]],AXIS["geodetic longitude (Lon)",east,ORDER[2],ANGLEUNIT["degree",0.0174532925199433]],ID["EPSG",4326]]],ABRIDGEDTRANSFORMATION["Transformation from GRS 1980(IUGG, 1980) to WGS84",METHOD["Position Vector transformation (geog2D domain)",ID["EPSG",9606]],PARAMETER["X-axis translation",0,ID["EPSG",8605]],PARAMETER["Y-axis translation",0,ID["EPSG",8606]],PARAMETER["Z-axis translation",0,ID["EPSG",8607]],PARAMETER["X-axis rotation",0,ID["EPSG",8608]],PARAMETER["Y-axis rotation",0,ID["EPSG",8609]],PARAMETER["Z-axis rotation",0,ID["EPSG",8610]],PARAMETER["Scale difference",1,ID["EPSG",8611]]]]'
     
     try:
@@ -66,77 +69,22 @@ def create_fishnet(collection_items_df, asset_num, dims, DEBUG=False):
 
         # Convert fishnet to gdf
         fishnet = gpd.GeoDataFrame(geom_array, columns=['geometry']).set_crs(in_crs)
-        #print("fishnet = ", fishnet.crs)
-        ### TO DO ###
-        # Write fishnet to disk
+
     except Exception as e:
         raise e
     
     return fishnet
-
-# def create_fishnet_on_collection_polygon(collection_polygon, dims, DEBUG=False):
-    
-#     in_crs = 'BOUNDCRS[SOURCECRS[PROJCRS["unnamed",BASEGEOGCRS["GRS 1980(IUGG, 1980)",DATUM["unknown",ELLIPSOID["GRS80",6378137,298.257222101,LENGTHUNIT["metre",1,ID["EPSG",9001]]]],PRIMEM["Greenwich",0,ANGLEUNIT["degree",0.0174532925199433]]],CONVERSION["unnamed",METHOD["Albers Equal Area",ID["EPSG",9822]],PARAMETER["Latitude of 1st standard parallel",50,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8823]],PARAMETER["Latitude of 2nd standard parallel",70,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8824]],PARAMETER["Latitude of false origin",40,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8821]],PARAMETER["Longitude of false origin",180,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8822]],PARAMETER["Easting at false origin",0,LENGTHUNIT["Meter",1],ID["EPSG",8826]],PARAMETER["Northing at false origin",0,LENGTHUNIT["Meter",1],ID["EPSG",8827]]],CS[Cartesian,2],AXIS["easting",east,ORDER[1],LENGTHUNIT["Meter",1]],AXIS["northing",north,ORDER[2],LENGTHUNIT["Meter",1]]]],TARGETCRS[GEOGCRS["WGS 84",DATUM["World Geodetic System 1984",ELLIPSOID["WGS 84",6378137,298.257223563,LENGTHUNIT["metre",1]]],PRIMEM["Greenwich",0,ANGLEUNIT["degree",0.0174532925199433]],CS[ellipsoidal,2],AXIS["geodetic latitude (Lat)",north,ORDER[1],ANGLEUNIT["degree",0.0174532925199433]],AXIS["geodetic longitude (Lon)",east,ORDER[2],ANGLEUNIT["degree",0.0174532925199433]],ID["EPSG",4326]]],ABRIDGEDTRANSFORMATION["Transformation from GRS 1980(IUGG, 1980) to WGS84",METHOD["Position Vector transformation (geog2D domain)",ID["EPSG",9606]],PARAMETER["X-axis translation",0,ID["EPSG",8605]],PARAMETER["Y-axis translation",0,ID["EPSG",8606]],PARAMETER["Z-axis translation",0,ID["EPSG",8607]],PARAMETER["X-axis rotation",0,ID["EPSG",8608]],PARAMETER["Y-axis rotation",0,ID["EPSG",8609]],PARAMETER["Z-axis rotation",0,ID["EPSG",8610]],PARAMETER["Scale difference",1,ID["EPSG",8611]]]]'
-    
-#     try:
-#         #asset = collection_items_df.iloc[asset_num]
-        
-#         asset = collection_polygon
-        
-#         print('\tasset id =', asset['id'])
-
-#         rows = asset['bands'][0]['dimensions'][0]
-#         cols = asset['bands'][0]['dimensions'][1]
-    
-#         print('\trows =', rows, 'cols = ', cols)
-    
-#         minx = (asset['bands'][0]['crs_transform'][2])
-#         miny = (asset['bands'][0]['crs_transform'][5])
-#         maxx = minx + (cols*30)
-#         maxy = miny - (rows*30)
-    
-#         if DEBUG:
-#             print('\tbbox Albers=', minx, miny, maxx, maxy)
-
-#         #create a fishnet: https://spatial-dev.guru/2022/05/22/create-fishnet-grid-using-geopandas-and-shapely/
-#         gdf_tile = gpd.GeoDataFrame(index=[0],geometry=[Polygon([[minx,miny],[minx,maxy],[maxx,maxy],[maxx, miny]])],crs=in_crs)
-    
-#         # Get the extent of the shapefile
-#         minX, minY, maxX, maxY = gdf_tile.total_bounds
-
-#         # Create a fishnet
-#         x, y = (minX, minY)
-#         geom_array = []
-
-#         # Polygon Size
-#         square_size = dims*30
-#         while y <= maxY:
-#             while x <= maxX:
-#                 geom = geometry.Polygon([(x,y), (x, y+square_size), (x+square_size, y+square_size), (x+square_size, y), (x, y)])
-#                 geom_array.append(geom)
-#                 x += square_size
-#             x = minX
-#             y += square_size
-
-#         # Convert fishnet to gdf
-#         fishnet = gpd.GeoDataFrame(geom_array, columns=['geometry']).set_crs(in_crs)
-#         #print("fishnet = ", fishnet.crs)
-#         ### TO DO ###
-#         # Write fishnet to disk
-#     except Exception as e:
-#         raise e
-    
-#     return fishnet
         
 def do_gee_download_by_subtile(SUBTILE_LOC, TILELOC, ASSET_PATH, 
                                #fishnet, asset_df, 
                                OUTDIR):
-    '''A wrapper of a gently modified ee_download.download_image_by_asset_path that downloads a subtile (based on a fishnet) of a GEE asset tile
+    '''
+    A wrapper of a gently modified ee_download.download_image_by_asset_path that downloads a subtile (based on a vector 'fishnet') of a GEE asset tile
     
     SUBTILE_LOC : the index number of the subtile that, with the fishnet, will define the subtile region of the GEE asset tile in which to download the data
-    TILELOC : the index of the GEE asset tile
-    ASSET_PATH : The GEE path to the image collection where the assets are stored
-    OUTDIR : the main ouput dir for this TILELOC into which many subdirs based on SUBTILE_LOC will be created
+    TILELOC     : the index of the GEE asset tile
+    ASSET_PATH  : The GEE path to the image collection where the assets are stored
+    OUTDIR      : the main ouput dir for this TILELOC into which many subdirs based on SUBTILE_LOC will be created
     
     ASSET_PATH gives you the following:
         fishnet : a vector fishnet based on the GEE asset tile bounds that defines the subtile regions
@@ -155,8 +103,11 @@ def do_gee_download_by_subtile(SUBTILE_LOC, TILELOC, ASSET_PATH,
     # if not os.path.exists(OUTDIR_TILE):
     #     os.makedirs(OUTDIR_TILE)
     
+    # Hard-coded tile size
+    # TODO: make into an argument for flexibility - too large and asset download will fail - too small and you have too many subtiles
+    TILE_SIZE_M = 500
     print(f'\n\tSubtile {SUBTILE_LOC} for tile loc {TILELOC} to subdir: {OUTDIR_TILE}')
-    fishnet_df = create_fishnet(asset_df, TILELOC, 500)
+    fishnet_df = create_fishnet(asset_df, TILELOC, TILE_SIZE_M)
     fishnet_df['subtile'] = fishnet_df.index
     fishnet_df['tile'] = TILELOC
     
@@ -205,17 +156,15 @@ def do_gee_download_by_subtile(SUBTILE_LOC, TILELOC, ASSET_PATH,
     return fails
     
 def main():
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("--subtile_loc", type=int, required=True, help="The subtile index number provided by a fishnet over the GEE asset tile.")
     parser.add_argument("--tile_loc", type=int, required=True, help="The tile index number of the GEE asset tile to import (based on df index (.iloc[x]) not tile id number)")
     parser.add_argument("--asset_path", type=str, required=True, default='projects/foreststructure/Circumboreal/S1_Composites_albers', help="The GEE path to the image collection where the assets are stored.")
-    #parser.add_argument("--fishnet_df", required=True, help="The data frame of the fishnet in 4326")
-    #parser.add_argument("--asset_df", required=True, help="The GEE asset data frame")
     parser.add_argument("--out_dir", type=str, required=True, help="The path where the subtile tifs will be imported")
     
     args = parser.parse_args()
     
-    #fails = do_gee_download_by_subtile(args.subtile_loc, args.tile_loc, args.fishnet_df, args.asset_df, args.out_dir)
     fails = do_gee_download_by_subtile(args.subtile_loc, args.tile_loc, args.asset_path, args.out_dir)
     
     return fails
