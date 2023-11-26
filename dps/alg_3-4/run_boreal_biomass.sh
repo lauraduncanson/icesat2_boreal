fa8488295712015c744c5ba2872098bf54c1ac60
lduncanson <lduncans@umd.edu>
2023-11-26 10:02:16 -0800

    solving dps install r packages

--- dps/alg_3-4/run_boreal_biomass.sh
+++ dps/alg_3-4/run_boreal_biomass.sh
@@ -55,8 +55,9 @@ MERGED_ATL08_CSV=$(ls ${OUTPUTDIR}/atl08_004_30m_filt_merge_neighbors* | head -1
 echo $MERGED_ATL08_CSV
 echo $ATL08_SAMPLE_CSV
 
-source activate base
-conda install --name base -c conda-forge r-rockchalk --solver=libmamba
+#source activate base
+#conda install --name base -c conda-forge r-rockchalk --solver=libmamba
+#mamba install r-rockchalk
 
 # Run mapBoreal with merged CSV as input
 Rscript ${basedir}/../../lib/mapBoreal_simple.R ${MERGED_ATL08_CSV} ${TOPO_TIF} ${LANDSAT_TIF} ${LC_TIF} ${DO_SLOPE_VALID_MASK} ${ATL08_SAMPLE_CSV} ${iters} ${ppside} ${minDOY} ${maxDOY} ${max_sol_el} ${expand_training} ${local_train_perc} ${min_n} ${boreal_vect_fn} ${predict_var}
