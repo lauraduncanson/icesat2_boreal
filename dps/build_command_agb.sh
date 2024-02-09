03075a5442347bfd54d824cf3532f672d68c9839
lduncanson <lduncans@umd.edu>
2024-02-09 13:25:08 -0800

    mamba to conda

--- dps/build_command_agb.sh
+++ dps/build_command_agb.sh
@@ -3,18 +3,17 @@ set -x
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
 
 #install requirements packages
-mamba env update -f ${basedir}/above_env.yml
+conda env update -f ${basedir}/above_env.yml
 
 pushd ${HOME}
 
 # Do not remove this (PMM Dec 2022)
-#source activate icesat2_boreal
+source activate icesat2_boreal
 
 # needed for ee asset export
 #mamba install --name icesat2_boreal -c conda-forge earthengine-api
 
 pip install git+https://github.com/MAAP-Project/maap-py.git#egg=maappy
 
-source activate base
-pip install geopandas
+source activate r
 pip3 install pyOpenSSL --upgrade
