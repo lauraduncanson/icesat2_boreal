49161bfd192cab817ffb8028a83b508208a3f437
montesano <paul.montesano@gmail.com>
2024-02-26 09:50:59 -0800

    dps prep

---
+++ dps/build_command_3.1.4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+basedir=$( cd "$(dirname "$0")" ; pwd -P )
+
+#install requirements packages
+conda env update -f ${basedir}/above_env_3.1.4.yml
+
+pushd ${HOME}
+
+# Do not remove this (PMM Dec 2022)
+source activate icesat2_boreal
+
+# needed for ee asset export
+#mamba install --name icesat2_boreal -c conda-forge earthengine-api
+
+pip install git+https://github.com/MAAP-Project/maap-py.git#egg=maappy
+
+source activate r
+pip3 install pyOpenSSL --upgrade
