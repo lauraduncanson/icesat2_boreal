c1f287d171a6cc3a3090ee37a04c3dd576332e93
montesano <paul.montesano@gmail.com>
2024-02-26 18:24:42 -0800

    dps prep

--- dps/build_command_3.1.4.sh
+++ dps/build_command_3.1.4.sh
@@ -2,6 +2,7 @@
 set -x
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
 
+source activate pangeo
 #install requirements packages
 conda env update -f ${basedir}/above_env_3.1.4.yml
 
@@ -15,5 +16,5 @@ source activate icesat2_boreal
 
 pip install git+https://github.com/MAAP-Project/maap-py.git#egg=maappy
 
-source activate r
+#source activate r
 pip3 install pyOpenSSL --upgrade
