d28f979919e0ce1d42df8b3ff879482837452bfe
lduncanson <lduncans@umd.edu>
2024-03-16 16:48:44 -0700

    new yml for r jobs

--- dps/build_command_agb.sh
+++ dps/build_command_agb.sh
@@ -3,7 +3,7 @@ set -x
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
 
 #install requirements packages
-conda env update -f ${basedir}/above_env_3.1.4.yml
+conda env update -f ${basedir}/above_env_r_3.1.4.yml
 
 pushd ${HOME}
 
